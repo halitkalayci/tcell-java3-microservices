2959527d98139b94aeb2a305f49531565844695e
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-04-25 18:54:26 +0300

    spring security base integration

---
+++ authserver/src/main/java/com/turkcell/authserver/core/configuration/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.turkcell.authserver.core.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
